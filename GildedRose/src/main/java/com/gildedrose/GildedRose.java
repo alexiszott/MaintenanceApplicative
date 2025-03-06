f66091fcffbf1963ff5dd32459b3269cd7f9d333
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2025-03-06 10:25:51 +0100

    test 100%

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,9 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String AGED_BRIE = "Aged Brie";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -14,52 +17,77 @@ class GildedRose {
     }
 
     private void handleItem(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+        switch (item.name) {
+            case AGED_BRIE:
+                handleAgedBrie(item);
+                break;
+            case SULFURAS:
+                handleSufuras(item);
+                break;
+            case BACKSTAGE:
+                handleBackstage(item);
+                break;
+            default:
+                handleNormalItems(item);
+                break;
+        }
+    }
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
+    private void handleAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    private void handleBackstage(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.sellIn < 11) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
+            }
+
+            if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void handleSufuras(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+
+    private void handleNormalItems(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
         }
     }
--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,6 +16,15 @@ class GildedRoseTest {
     }
 
     @Test
+    void testNormalItemQualityZero() {
+        Item[] items = new Item[] { new NormalItem("Normal Item", 10, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
     void testAgedBrie() {
         Item[] items = new Item[] { new AgedBrie("Aged Brie", 10, 10) };
         GildedRose app = new GildedRose(items);
@@ -34,6 +43,15 @@ class GildedRoseTest {
     }
 
     @Test
+    void testAgedBrieInf() {
+        Item[] items = new Item[] { new AgedBrie("Aged Brie", -10, 70) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-11, items[0].sellIn);
+        assertEquals(70, items[0].quality);
+    }
+
+    @Test
     void testSulfuras() {
         Item[] items = new Item[] { new Sulfuras("Sulfuras, Hand of Ragnaros", 10, 80) };
         GildedRose app = new GildedRose(items);
@@ -43,12 +61,39 @@ class GildedRoseTest {
     }
 
     @Test
+    void testSulfurasInf() {
+        Item[] items = new Item[] { new Sulfuras("Sulfuras, Hand of Ragnaros", -10, -80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-10, items[0].sellIn);
+        assertEquals(-79, items[0].quality);
+    }
+
+    @Test
     void testBackstagePasses() {
         Item[] items = new Item[] { new BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(9, items[0].sellIn);
         assertEquals(22, items[0].quality);
+    }
+
+    @Test
+    void testBackstageNotPassesSellIn() {
+        Item[] items = new Item[] { new BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 20, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19, items[0].sellIn);
+        assertEquals(21, items[0].quality);
+    }
+
+    @Test
+    void testBackstageNotPassesQuality() {
+        Item[] items = new Item[] { new BackstagePasses("Backstage passes to a TAFKAL80ETC concert", 5, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(4, items[0].sellIn);
+        assertEquals(50, items[0].quality);
     }
 
     @Test
